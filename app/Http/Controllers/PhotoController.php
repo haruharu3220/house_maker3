6f7a4327062eb89e4743496a2af7a3002fd7b605
hayaharu3220 <hayato.314.nico@gmail.com>
2023-03-22 15:58:36 +0000

    Photoを投稿した時、ログインユーザをDBに登録する処理を追加

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+
+
 
 class PhotoController extends Controller
 {
     //
+    public function store(Request $request)
+    {
+    // バリデーション
+        // 今回は特に不要
+    // バリデーション:エラー
+        // if ($validator->fails()) {
+        //   return redirect()
+        //     ->route('tweet.create')
+        //     ->withInput()
+        //     ->withErrors($validator);
+        // }
+
+    // 編集 フォームから送信されてきたデータとユーザIDをマージし，DBにinsertする
+    $data = $request->merge(['user_id' => Auth::user()->id])->all();
+    $result = Photo::create($data);
+
+    // tweet.index」にリクエスト送信（一覧ページに移動）
+    return redirect()->route('tweet.index');
+    }
+    
 }
