644a434c61a3e70fd33541e27059400122237c41
hayaharu3220 <hayato.314.nico@gmail.com>
2023-03-22 16:14:54 +0000

    画像一覧画面の作成

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
-
+use Validator;
+use App\Models\Photo;
 
 
 class PhotoController extends Controller
 {
-    //
+    
+    //index
+    public function index(){
+        return response()->view('photo.index');
+    }
+    
+    //create
+    public function create()
+    {
+        return response()->view('photo.create');
+    }
+
+    
+    
+    //store
     public function store(Request $request)
     {
     // バリデーション
@@ -29,5 +44,8 @@ class PhotoController extends Controller
     // tweet.index」にリクエスト送信（一覧ページに移動）
     return redirect()->route('tweet.index');
     }
+    
+    
+    
     
 }
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
+//RESTfulルートを自動的に生成
+Route::resource('photo', PhotoController::class);
 
 Route::get('/', function () {
     return view('welcome');
