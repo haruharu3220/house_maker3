a2ce18b3698b4cdfd6a89ac9de2eed2362e97bcc
hayaharu3220 <hayato.314.nico@gmail.com>
2023-03-23 16:09:53 +0000

    複合検索でカテゴリ別に画像を並び替える

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import './test.js';
 
 window.Alpine = Alpine;
 
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,21 @@
-const mix = require('laravel-mix');
+//既存のコード
+// const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
+
+// mix.js('resources/js/app.js', 'public/js')
+//     .postCss('resources/css/app.css', 'public/css', [
+//         //
+//     ]);
+
+
+
+const mix = require('laravel-mix');
+const { vitePlugin } = require('laravel-mix-vite');
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+   .postCss('resources/css/app.css', 'public/css', [
+       require('tailwindcss'),
+   ])
+   .vite({
+       plugins: [vitePlugin()],
+   });
