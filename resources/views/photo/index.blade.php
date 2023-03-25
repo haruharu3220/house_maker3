3559be2e60f54846b22a8cff94693c27c1fe7fc2
hayaharu3220 <hayato.314.nico@gmail.com>
2023-03-25 07:20:33 +0000

    画像が５枚以上あっても重ならないように修正

--- resources/views/photo/index.blade.php
+++ resources/views/photo/index.blade.php
@@ -32,37 +32,6 @@
       </h2>
     </x-slot>
     
-    <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:w-10/12 md:w-8/10 lg:w-8/12">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-          <div class="p-6 bg-white dark:bg-gray-800 border-b border-grey-200 dark:border-gray-800">
-            <table class="text-center w-full border-collapse">
-              <thead>
-                <tr>
-                  <th class="py-4 px-6 bg-gray-lightest dark:bg-gray-darkest font-bold uppercase text-lg text-gray-dark dark:text-gray-200 border-b border-grey-light dark:border-grey-dark">tweet</th>
-                </tr>
-              </thead>
-              <tbody>
-                @foreach ($photos as $photo)
-                <tr class="hover:bg-gray-lighter">
-                  <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">
-                    <h3 class="text-left font-bold text-lg text-gray-dark dark:text-gray-200">{{$photo->image}}</h3>
-                    <img src="{{ asset('storage/image/'.$photo->image)}}"　class="mx-auto" style="height:300px;">
-                    <div class="flex">
-                      <!-- 更新ボタン -->
-                      <!-- 削除ボタン -->
-                    </div>
-                  </td>
-                </tr>
-                @endforeach
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-      
-      
     <ul class="sort-btn"> 
       <li>
         <dl>
@@ -100,46 +69,20 @@
       </li>
   <!--/sort-btn--></ul>
   
-  <ul class="grid"><!--1番外側のタグにgrid というクラス名を付与。-->
+  <ul class="grid">
     @foreach ($photos as $photo)
       <!--itemというクラス名と並び替え基準となる複数のクラス名（チェックボックスのクラス名と同じ名前）を付与。-->
-    <li class="item {{$photo->image}} {{$photo->id}}">
+      <li class="item {{$photo->image}} {{$photo->id}}">
         <!--内側のdivには高さを維持するためにitem-contentというクラス名をつける。-->
-      <div class="item-content">
-          <tr class="hover:bg-gray-lighter">  
-            <td class="py-4 px-6 border-b border-gray-light dark:border-gray-600">
+        <div class="item-content">
+          <a href="{{$photo->image}}" data-fancybox="group2" data-caption="グループ2キャプション">
               <img src="{{ asset('storage/image/'.$photo->image)}}" class="modal-trigger mx-auto" >
-            </td>
-          </tr>
-      </div>
-    </li>
+          </a>
+        </div>
+      </li>
     @endforeach
   </ul>
-
-<!--        <div class="item-content"><!--内側のdivには高さを維持するためにitem-contentというクラス名をつける。-->
-<!--          <a href="img/01.jpg" data-fancybox="group1" data-caption="グループ1キャプション">-->
-<!--            <img src="img/01.jpg" alt="">-->
-<!--          </a><!--複数画像をグループ化してサムネイル表示させたい場合は、datafancybox="半角英数字で同一のグループ名"、キャプションを入れたい場合はdata-caption="キャプションタイトル"を設定する。-->
-
-
-
-<!-->-->
-<!--      <li class="item cat01 cat02 color01">-->
-<!--        <div class="item-content">-->
-<!--          <a href="img/02.jpg" data-fancybox="group2" data-caption="グループ2キャプション">-->
-<!--            <img src="img/02.jpg" alt="">-->
-<!--          </a>-->
-<!--        </div>-->
-<!--      </li>-->
-<!--      <li class="item cat03 color02">-->
-<!--        <div class="item-content">-->
-<!--          <a href="img/03.jpg" data-fancybox="group3" data-caption="グループ3キャプション">-->
-<!--            <img src="img/03.jpg" alt="">-->
-<!--          </a>-->
-<!--        </div>-->
-<!--      </li>-->
-<!--    </ul>-->
-
+  
 <!-- モーダルウィンドウ -->
 <div class="modal-wrapper">
   <div id="modal">
